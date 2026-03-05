8994de2da7e5241f5194a455050165a7ab34b415
iiTzSebas <jtoscanoprieto@hotmail.com>
2026-03-05 13:37:23 -0500

    DATOS DE ESTUDIANTES EN ADMIN

--- AppPagina/models.py
+++ AppPagina/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+
 # Create your models here.
+class estudiante(models.Model):
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    apellido = models.CharField(max_length=50, verbose_name="Apellido")
+    foto = models.ImageField(upload_to='fotos_estudiantes/', verbose_name="Foto", null=True)
+    clase = models.TextField(max_length=100, verbose_name="Clase", null=True) 
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
